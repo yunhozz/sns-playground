ba6c23869cee54120daa422ef0b0a99588341686
Yunho Park <qkrdbsgh0111@gmail.com>
2025-11-18 23:11:38 +0900

    eslint 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,10 @@ export default defineConfig([
             ecmaVersion: 2020,
             globals: globals.browser,
         },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "react-refresh/only-export-components": "off"
+        }
     },
 ]);
