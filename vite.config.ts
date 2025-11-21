4cfae0f710f18182bf5e275cdcd05e34bc9916d4
Yunho Park <qkrdbsgh0111@gmail.com>
2025-11-21 22:14:01 +0900

    json-server 구성

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,10 @@ export default defineConfig({
         alias: {
             "@": path.resolve(__dirname, "./src")
         }
+    },
+    server: {
+        watch: {
+            ignored: ["**/server/**"]
+        }
     }
 });
