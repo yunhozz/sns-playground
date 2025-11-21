392720f38b8cd2d24dabaefb52a3bae385eeb9f1
Yunho Park <qkrdbsgh0111@gmail.com>
2025-11-21 23:09:03 +0900

    Tanstack Query 의존성 추가 및 QueryClient 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
-        <App/>
+        <QueryClientProvider client={queryClient}>
+            <App/>
+        </QueryClientProvider>
     </BrowserRouter>
 );
