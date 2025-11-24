0b89eca05e09d0b04ae87899bb865a9bdf707e78
Yunho Park <qkrdbsgh0111@gmail.com>
2025-11-24 22:04:14 +0900

    TodosList 페이지 보완

--- src/pages/todo-list-page.tsx
+++ src/pages/todo-list-page.tsx
@@ -1,15 +1,13 @@
 import TodoEditor from "@/components/todo-list/todo-editor.tsx";
 import TodoItem from "@/components/todo-list/todo-item.tsx";
-import { useTodosDataV2 } from "@/hooks/queries/use-todos.data.ts";
+import { useTodosData } from "@/hooks/queries/use-todos-data.ts";
 
 export default () => {
     // const todos = useTodos();
+    const { data: todos, isLoading, error } = useTodosData();
 
-    // const { todos, isLoading, error } = useTodosDataV1();
-    const { data: todos, isLoading, error } = useTodosDataV2();
-
-    if (error) return <div>오류가 발생했습니다.</div>;
     if (isLoading) return <div>로딩 중 입니다...</div>;
+    if (error) return <div>오류가 발생했습니다.</div>;
 
     return (
         <div className={"p-5 flex flex-col gap-5"}>
