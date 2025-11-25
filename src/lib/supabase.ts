42e7b84abadad254fcc4a7d820d41837c20d95f7
Yunho Park <qkrdbsgh0111@gmail.com>
2025-11-25 23:05:02 +0900

    Supabase 설정

---
+++ src/lib/supabase.ts
@@ -0,0 +1,9 @@
+import type { Database } from "@/database.types.ts";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+
+export default supabase;
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,9 @@
+import type { Database } from "@/database.types.ts";
+
 export interface ITodo {
     id: string;
     content: string;
     isDone: boolean;
 }
+
+export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
