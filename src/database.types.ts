281eba7a22fff7c432dd9bc9ac621d1d95d2651c
Yunho Park <qkrdbsgh0111@gmail.com>
2025-12-04 22:49:14 +0900

    관계 정보 추가

--- src/database.types.ts
+++ src/database.types.ts
@@ -39,7 +39,15 @@ export type Database = {
                     image_urls?: string[] | null
                     like_count?: number
                 }
-                Relationships: []
+                Relationships: [
+                    {
+                        foreignKeyName: "post_author_id_fkey"
+                        columns: ["author_id"]
+                        isOneToOne: false
+                        referencedRelation: "profile"
+                        referencedColumns: ["id"]
+                    },
+                ]
             }
             profile: {
                 Row: {
